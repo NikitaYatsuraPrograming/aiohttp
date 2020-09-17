f20fc28ed848c3ba609c9a2f4da9b0ca109f4bdc
Nikita <nikitayatsuraprograming@gmail.com>
2020-09-17 19:11:16 +0300

    wrote middelware it hangles errors 400, 500

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
 
-from aiohttpdemo_polls.settings import config
+from aiohttpdemo_polls.settings import config, BASE_DIR
 from aiohttpdemo_polls.routes import setup_routes
 from aiohttpdemo_polls.db import close_pg, init_pg
+from middlewares import setup_middlewares
 
 app = web.Application()
-setup_routes(app)
 app['config'] = config
-app.on_cleanup.append(init_pg)
-app.on_cleanup.append(close_pg)
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttpdemo_polls' / 'templates')))
 
-web.run_app(app, port=9000)
+setup_routes(app)
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
+setup_middlewares(app)
+
+web.run_app(app, port=9090)
 
